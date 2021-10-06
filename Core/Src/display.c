87b5b155f14f635316942fac9c3e54f6a38a80b7
Sakura <sakura@sakuraa.xyz>
2021-10-06 22:09:03 +0200

    Video Driver (WIP)

--- Core/Src/display.c
+++ Core/Src/display.c
@@ -1,8 +1,27 @@
-/*
- * display.c
- *
- *  Created on: Sep 27, 2021
- *      Author: Sakura
- */
+// SakuraOS Display Driver
+#include "display.h"
+#include "stdint.h"
 
+uint16_t const SCREEN_WIDTH = 320;
+uint16_t const SCREEN_HEIGHT = 240;
 
+void StartBacklight(){
+	HAL_GPIO_WritePin(LCD_Backlight_GPIO_Port, LCD_Backlight_Pin, GPIO_PIN_SET);
+}
+
+void StopBacklight(){
+	HAL_GPIO_WritePin(LCD_Backlight_GPIO_Port, LCD_Backlight_Pin, GPIO_PIN_RESET);
+}
+
+void ClearScreen() {
+	HAL_GPIO_WritePin(LCD_Reset_GPIO_Port, LCD_Reset_Pin, GPIO_PIN_SET);
+	HAL_Delay(25);
+	HAL_GPIO_WritePin(LCD_Reset_GPIO_Port, LCD_Reset_Pin, GPIO_PIN_RESET);
+	HAL_Delay(50);
+}
+
+void Display_Init() {
+	HAL_GPIO_WritePin(LCD_Power_GPIO_Port, LCD_Power_Pin, GPIO_PIN_SET);
+	ClearScreen();
+	StartBacklight();
+}
